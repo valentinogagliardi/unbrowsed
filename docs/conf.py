22a149b0eb4234a50684f129a420a6ad64b54f3c
Valentino Gagliardi <valentino@valentinog.com>
2025-03-09 11:05:44 +0100

    fix style

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ release = "0.1.0a9"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
+    "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
